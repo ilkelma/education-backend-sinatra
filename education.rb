0caca3a2c13a862098c7a0736eea6e73c6d84abb
ilkelma <ilkelma@users.noreply.github.com>
2017-04-30 15:33:43 -0400

    add something less hello worldy

--- education.rb
+++ education.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
 get '/' do
-    'Hello 世界'
+    'This is the api root. At the moment there\'s not much of use here. Why not try a resource like /education?'
+end
+
+get '/education' do
+    'Well, we think he may have learned something but we\'re not sure'
 end
